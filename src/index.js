03d193c440f9ef64c9c38d18c94783a1459b0488
DatLB1 <doglam97@gmail.com>
2023-10-08 11:13:03 +0700

    second commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const hbs = require('express-handlebars');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Templete engine
+app.engine('.hbs', hbs.engine(
+    {
+        extname: '.hbs'
+    }));
+
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
+
